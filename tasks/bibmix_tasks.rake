8a1601bbb945e1c0f1bd9fa686f6a22da66a422d
Bas Wenneker <bas@bas-wennekers-macbook-pro.local>
2010-04-27 18:20:02 +0200

    First commit.

---
+++ tasks/bibmix_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :bibmix do
+#   # Task goes here
+# end
---
+++ test/bibmix_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class BibmixTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
