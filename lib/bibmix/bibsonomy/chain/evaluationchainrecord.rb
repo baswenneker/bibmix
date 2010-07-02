03faad081acae469185e6fcc412bdc6b63e0b44b
Bas Wenneker <bas@bas-wennekers-macbook-pro.local>
2010-07-02 09:43:23 +0200

    Fixed chain_test unittest by fixing the template_macros.xls file.

--- lib/bibmix/bibsonomy/chain/evaluationchainrecord.rb
+++ lib/bibmix/bibsonomy/chain/evaluationchainrecord.rb
@@ -30,8 +30,10 @@ module Bibmix
 				authorquery_record = get_record(Chain::STATUS_AUTHOR_MERGED)
 				attr_record = Bibmix::Bibsonomy::Record.new
 				
+				puts template_file
+				puts File.exists?(template_file)
 				# initialize the spreadsheet
-			  workbook = Spreadsheet.open(template_file, 'w+')
+			  workbook = Spreadsheet.open(template_file)
 				
 				# set coordinates
 				co_date = {:x => 1, :y => 0}
